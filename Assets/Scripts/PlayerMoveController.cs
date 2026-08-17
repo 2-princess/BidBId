a5b715f343a04778e1f3686f1d6eadd363e5dbad
2Princess <ww@jeongdawon-ui-MacBookPro.local>
2026-08-18 04:59:46 +0900

    run

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 
 public class PlayerMoveController : NetworkBehaviour
 {
-    float speed = 4f;
     public Rigidbody playerRigid;
     public Animator animator;
     public Transform skull;
+    public bool isGround = false;
+    float speed = 4f;
+    public PlayerState state;
+    public enum PlayerState
+    {
+        Idle,
+        Walk,
+        Run
+    }
 
     void FixedUpdate()
     {
@@ -15,16 +23,47 @@ public class PlayerMoveController : NetworkBehaviour
         float v = Input.GetAxis("Vertical");
         Vector3 moving = new Vector3(h * speed, playerRigid.linearVelocity.y, v * speed);
         playerRigid.linearVelocity = moving;
+
         if (h != 0 || v != 0)
         {
             Vector3 dir = new Vector3(h, 0, v);
             Quaternion targetRot = Quaternion.LookRotation(dir);
             skull.rotation = targetRot;
-            animator.SetBool("Walk", true);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 6f;
+                state = PlayerState.Run;
+            }
+            else
+            {
+                speed = 4f;
+                state = PlayerState.Walk;
+            }
         }
         else
         {
-            animator.SetBool("Walk", false);
+            state = PlayerState.Idle;
+        }
+        ani(state);
+    }
+
+    void ani(PlayerState state)
+    {
+        switch (state)
+        {
+            case PlayerState.Idle:
+                animator.SetBool("Walk", false);
+                animator.SetBool("Run", false);
+                break;
+            case PlayerState.Walk:
+                animator.SetBool("Walk", true);
+                animator.SetBool("Run", false);
+                break;
+            case PlayerState.Run:
+                animator.SetBool("Walk", false);
+                animator.SetBool("Run", true);
+                break;
+
         }
     }
 }
